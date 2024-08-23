79bb9d386c15e6065130e0035822f2e926683b4e
Aditya Patange <contact.adityapatange@gmail.com>
2024-08-23 22:23:51 +0530

    test/calculator: fix import statement

--- src/lib/calculator.test.ts
+++ src/lib/calculator.test.ts
@@ -1,6 +1,6 @@
 describe("Calculator Tests", () => {
   it("should be importable", async () => {
-    const calculator = await import("./calculator.ts");
+    const calculator = await import("./calculator");
     expect(calculator).toBeDefined();
   });
 });

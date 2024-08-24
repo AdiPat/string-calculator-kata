16c4d3f7452651caccd1129f7c10f3500179eeb0
Aditya Patange <contact.adityapatange@gmail.com>
2024-08-24 07:38:26 +0530

    components: update user input as per new tests
    - include button to compute
    - include input box state management

--- src/components/user-input.tsx
+++ src/components/user-input.tsx
@@ -1,6 +1,34 @@
+import { add } from "@/lib/calculator";
 import { Button, Input } from "@nextui-org/react";
 
-export function UserInput() {
+interface UserInputProps {
+  value: string;
+  setResult: (value: number) => void;
+  setValue: (value: string) => void;
+  setShowResult: (showResult: boolean) => void;
+  setError: (error: string) => void;
+}
+
+export function UserInput({
+  value,
+  setResult,
+  setValue,
+  setShowResult,
+  setError,
+}: UserInputProps) {
+  const onCalculate = () => {
+    try {
+      setShowResult(false);
+      const result = add(value);
+      setResult(result);
+      setShowResult(true);
+    } catch (error) {
+      setResult(0);
+      setError("Error when computing sum. Please check your input.");
+      setShowResult(false);
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-4">
       <div
@@ -10,8 +38,17 @@ export function UserInput() {
         <label className="text-white text-lg mb-4" htmlFor="numbers">
           Enter string to compute sum:
         </label>
-        <Input placeholder="Enter comma separated numbers" />
-        <Button size="lg" color="primary" className="mt-4">
+        <Input
+          placeholder="Enter comma separated numbers"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Button
+          size="lg"
+          color="primary"
+          className="mt-4"
+          onClick={onCalculate}
+        >
           Calculate
         </Button>
       </div>

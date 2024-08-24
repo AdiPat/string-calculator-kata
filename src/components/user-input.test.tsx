4d546a31fd5fd7a0fd55cb23183bde1f73104f15
Aditya Patange <contact.adityapatange@gmail.com>
2024-08-24 07:37:40 +0530

    components/tests: update user input test with props

--- src/components/user-input.test.tsx
+++ src/components/user-input.test.tsx
@@ -4,24 +4,88 @@ import { UserInput } from "./user-input";
 
 describe("UserInput Tests", () => {
   it("renders a user input with a label, input and button", async () => {
-    const { container } = render(<UserInput />);
+    const { container } = render(
+      <UserInput
+        value={""}
+        setResult={function (value: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        setValue={function (value: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        setShowResult={function (showResult: boolean): void {
+          throw new Error("Function not implemented.");
+        }}
+        setError={function (error: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
     expect(container).toMatchSnapshot();
   });
 
   it("renders a user input with a label", async () => {
-    render(<UserInput />);
-    const label = screen.getByText(/Enter string to calculate sum:/i);
+    const { container } = render(
+      <UserInput
+        value={""}
+        setResult={function (value: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        setValue={function (value: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        setShowResult={function (showResult: boolean): void {
+          throw new Error("Function not implemented.");
+        }}
+        setError={function (error: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
+    const label = screen.getByText(/Enter string to compute sum:/i);
     expect(label).toBeInTheDocument();
   });
 
   it("renders a user input with a placeholder", async () => {
-    render(<UserInput />);
+    const { container } = render(
+      <UserInput
+        value={""}
+        setResult={function (value: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        setValue={function (value: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        setShowResult={function (showResult: boolean): void {
+          throw new Error("Function not implemented.");
+        }}
+        setError={function (error: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
     const input = screen.getByPlaceholderText(/Enter comma separated numbers/i);
     expect(input).toBeInTheDocument();
   });
 
   it("renders a user input with a button", async () => {
-    render(<UserInput />);
+    const { container } = render(
+      <UserInput
+        value={""}
+        setResult={function (value: number): void {
+          throw new Error("Function not implemented.");
+        }}
+        setValue={function (value: string): void {
+          throw new Error("Function not implemented.");
+        }}
+        setShowResult={function (showResult: boolean): void {
+          throw new Error("Function not implemented.");
+        }}
+        setError={function (error: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+    );
     const button = screen.getByText(/Calculate/i);
     expect(button).toBeInTheDocument();
   });
